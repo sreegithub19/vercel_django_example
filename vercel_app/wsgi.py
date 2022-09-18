d756a127b625c5ec03d808378f0d9670dedf7aaf
K <sreedhar.k@rakuten.com>
2022-09-18 20:18:13 +0530

    Initial commit

--- vercel_app/wsgi.py
+++ vercel_app/wsgi.py
@@ -5,6 +5,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+
+In case of "Application crashed" error:
+      (Check logs) https://vercel.com/sreegithub19/vercel-django-example/C3yDuT4LSwyDaT9gG4iuVekHDNae/functions
+      Solution: https://stackoverflow.com/questions/73706065/missing-variable-handler-or-app-in-file-project-wsgi-py
+  
 """
 
 import os
