57c62bc16dc0a0e0ee90bd7ba27a2cdf0da099c5
K <sreedhar.k@rakuten.com>
2022-09-19 02:33:29 +0530

    Initial commit

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -7,4 +7,18 @@ from example.views import *
 urlpatterns = [
     path('', index),
     path('chess', chess),
+    path('calculator', calculator),
+    path('maze', maze),
+    path('sass_', sass_),
+    path('codepen', codepen),
+    path('tilt_maze', tilt_maze),
+    path('dino', dino),
+    path('solitaire', solitaire),
+    path('sudoku', sudoku),
+    path('puzzles', puzzles),
+    path('tic_tac_toe', tic_tac_toe),
+    path('clock', clock),
+    path('hangman', hangman),
+    path('virtual_keyboard', virtual_keyboard),
+    
 ]
