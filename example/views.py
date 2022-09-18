5601cbdaef8e6ebf3323d32774780535fa0f4b3d
K <sreedhar.k@rakuten.com>
2022-09-18 19:05:56 +0530

    Initial commit

---
+++ example/views.py
@@ -0,0 +1,29 @@
+# vercel-django-example/example/views.py
+# https://github.com/jayhale/vercel-django-example/blob/main/example/views.py#L6
+
+from datetime import datetime
+from django.http import HttpResponse
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+
+def applications(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
---
+++ vercel_app/urls.py
@@ -0,0 +1,10 @@
+# vercel-django-example/example/urls.py
+# https://github.com/jayhale/vercel-django-example/blob/main/example/urls.py#L7
+
+from django.urls import path
+from example.views import *
+
+urlpatterns = [
+    path('', index),
+    path('applications', applications),
+]
